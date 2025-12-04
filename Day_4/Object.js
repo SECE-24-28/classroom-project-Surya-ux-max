1a274b7ffbb035a37328cca399a425b61f97ad0e
Surya-ux-max <surprakas14@gmail.com>
2025-12-04 10:34:29 +0530

    day 4

--- Day_4/Object.js
+++ Day_4/Object.js
@@ -7,10 +7,35 @@
 // items2.splice(0,1);
 // console.log(items2);
 
-let items3 = ["apple", "banana", "orange"];
-items3.push("grape");
-console.log(items3);
+// let items3 = ["apple", "banana", "orange"];
+// items3.push("grape");
+// console.log(items3);
 
-let items2 = ["apple", "banana", "orange"];
-items2.splice(1,0,"kiwi");
-console.log(items2);
+// let items2 = ["apple", "banana", "orange"];
+// items2.splice(1,0,"kiwi");
+// console.log(items2);
+
+// let items4 = ["apple", "banana", "orange"];
+// items4.forEach(function(item){
+//     console.log(item);
+// });
+
+// let nums = [10, 20, 30, 40];
+
+// nums.forEach(num =>{
+//     console.log(num);
+// });
+
+
+// let colors = ["red", "green", "blue"];
+
+// colors.forEach((color, index) => {
+//     console.log(index + ": " + color);
+// });
+
+// let fruties = ["apple","banana","grape"];
+
+// fruties.forEach((fruties,index) =>{
+//     console.log(index+":"+fruties);
+// });
+
