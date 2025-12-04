abe7a29ea3ca5cb349306f5e004b80a134883e37
Surya-ux-max <surprakas14@gmail.com>
2025-12-04 10:04:01 +0530

    day 4

---
+++ Day_4/Object.js
@@ -0,0 +1,16 @@
+// let items = ["apple", "banana"];
+// items.push("orange"); // ["apple", "banana", "orange"]
+// console.log(items);
+
+
+// let items2 = ["apple", "banana", "orange"];
+// items2.splice(0,1);
+// console.log(items2);
+
+let items3 = ["apple", "banana", "orange"];
+items3.push("grape");
+console.log(items3);
+
+let items2 = ["apple", "banana", "orange"];
+items2.splice(1,0,"kiwi");
+console.log(items2);
