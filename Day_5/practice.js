45a94158439ba924a8b354049d3ec86d4ec72c65
Surya-ux-max <surprakas14@gmail.com>
2025-12-05 10:13:07 +0530

    day 5

--- Day_5/practice.js
+++ Day_5/practice.js
@@ -1,10 +1,39 @@
-let object ={
-    name:"Surya",
-    age:22,
-    place:"Chennai"
-}
+// let object ={
+//     name:"Surya",
+//     age:22,
+//     place:"Chennai"
+// }
 
-let{name,age,place} = object;
-console.log(name);
-console.log(age);
-console.log(place);
+// let{name,age,place} = object;
+// console.log(name);
+// console.log(age);
+// console.log(place);
+
+// set interval 
+// console.log("Set Interval Demo:");
+// setInterval(()=>{
+//     console.log("Hello World");
+// },2000)
+// console.log("Set Timeout Demo:");
+// setTimeout(()=>{
+//     console.log("Hello World");
+// },2000) // it will execute only once
+// // callback function
+// console.log("Callback Function Demo:");
+// setTimeout(()=>{
+//     console.log("process 2");
+//     setTimeout(()=>{
+//         console.log("process 3");
+// },2000)},2000);
+// console.log("process 1");
+// // promises
+// console.log("Promise Demo:");
+
+let res = fetch("https://jsonplaceholder.typicode.com/users")
+  .then(res=>{
+    return res.json();
+  })
+  .then(user=>{
+    console.log(user)
+  })
+  console.log(res);
