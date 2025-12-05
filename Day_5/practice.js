959ee0a7528737c10ce360b3f1e4261d62ad26b6
Surya-ux-max <surprakas14@gmail.com>
2025-12-05 11:24:30 +0530

    day 5

--- Day_5/practice.js
+++ Day_5/practice.js
@@ -28,17 +28,19 @@
 // console.log("process 1");
 // // promises
 // console.log("Promise Demo:");
-let res=fetch('https://jsonplaceholder.typicode.com/posts')
-.then(res=>{
-    return res.json();
-})
-.then(post=>{
-    console.log(post);
-})
-console.log(res)
-let fetch_API=async()=>{
-    const response=await fetch('https://jsonplaceholder.typicode.com/posts');
-    const data=await response.json();
-    console.log(data);
-}
-fetch_API();
+// let res=fetch('https://jsonplaceholder.typicode.com/posts')
+// .then(res=>{
+//     return res.json();
+// })
+// .then(post=>{
+//     console.log(post);
+// })
+// console.log(res)
+// let fetch_API=async()=>{
+//     const response=await fetch('https://jsonplaceholder.typicode.com/posts');
+//     const data=await response.json();
+//     console.log(data);
+// }
+// fetch_API();
+
+
