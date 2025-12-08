53231d897ca6ceb466fb513691601241d504ccbc
Surya-ux-max <surprakas14@gmail.com>
2025-12-08 10:32:25 +0530

    Button adding

--- Day_8/react_app/src/App.jsx
+++ Day_8/react_app/src/App.jsx
@@ -3,23 +3,23 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+function MyButton() {
+  
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <h1>React is the Library , Not a Framework</h1>
+    <button>
+      MyButton
+    </button>
     </>
   )
 }
 
-export default App
+export default function App(){
+  return (
+    <div>
+      <h1>Welcome to App</h1>
+      <MyButton />
+    </div>
+  )
+}
